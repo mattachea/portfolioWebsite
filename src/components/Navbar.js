ae01b965e7696089fc3f36102270196fbea5d4d7
Matthew Chea <mattachea@gmail.com>
2020-10-26 03:19:09 -0400

    tweaked responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,5 @@
 import "./Navbar.css";
-
 import React, { useState } from "react";
-import ToysIcon from "@material-ui/icons/Toys";
 import ReorderIcon from "@material-ui/icons/Reorder";
 import CloseIcon from "@material-ui/icons/Close";
 
@@ -14,9 +12,8 @@ export default function Navbar() {
   return (
     <div className="navbar">
       <div className="nav-title">
-        <a href="/#home" onClick={closeOnClick} className="nav-item">
-          <ToysIcon className="menu-icon" />
-          Matthew Chea
+        <a href="/#home" onClick={closeOnClick} className="nav-icon">
+          MC
         </a>
         <div className="menu-icon" onClick={toggleOpen}>
           {isOpen ? <CloseIcon /> : <ReorderIcon />}
@@ -25,29 +22,17 @@ export default function Navbar() {
 
       <ul className={isOpen ? "nav-menu active" : "nav-menu"}>
         <li>
-          <a
-            href="/#home"
-            onClick={closeOnClick}
-            className="nav-item list-item"
-          >
+          <a href="/#home" onClick={closeOnClick} className="nav-item">
             Home
           </a>
         </li>
         <li>
-          <a
-            href="/#projects"
-            onClick={closeOnClick}
-            className="nav-item list-item"
-          >
+          <a href="/#projects" onClick={closeOnClick} className="nav-item">
             Projects
           </a>
         </li>
         <li>
-          <a
-            href="/#contact"
-            onClick={closeOnClick}
-            className="nav-item list-item"
-          >
+          <a href="/#contact" onClick={closeOnClick} className="nav-item">
             Contact
           </a>
         </li>
@@ -56,7 +41,7 @@ export default function Navbar() {
             href="https://drive.google.com/file/d/1VmSSyy2tCFFy-a3yBTCnQ-DQSZaVwrpF/view?usp=sharing"
             target="blank"
             onClick={closeOnClick}
-            className="nav-item list-item"
+            className="nav-item"
           >
             Resume
           </a>
